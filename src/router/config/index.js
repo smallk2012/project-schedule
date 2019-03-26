bfc5be768b4b6cf25893ca68768e48e3cb89dec3
cC <>
2019-03-26 19:37:19 +0800

    cc

--- src/router/config/index.js
+++ src/router/config/index.js
@@ -1,7 +1,7 @@
 export default {
     routes: [
         {
-            path: '/',
+            path: '/hello',
             name: 'HelloWorld',
             component: () => import('@/components/HelloWorld')
         },
@@ -18,7 +18,7 @@ export default {
         {
             path: '*',
             redirect: {
-                path: '/'
+                path: '/home'
             }
         }
     ]
--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,9 @@ import config from './config'
 
 Vue.use(Router)
 const router = new Router(config)
-let _this = Vue.prototype
+// let _this = Vue.prototype
 router.beforeEach((to, from, next) => {
     // 这里写你的一些页面跳转控制逻辑
-    _this.$log(to)
-    if (to.path == '/login') {
-        _this.$session.del('token')
-        next()
-    } else {
-        if (to.query.token || _this.$session.get('token')) {
-            // 登录了
-            next()
-        } else {
-            next('/login')
-        }
-    }
+    next()
 })
 export default router
